d0167a3942dcac391c59070700e73e0e766978fe
Fernando Michea <fmicheam@gmail.com>
2024-08-04 14:15:45 -0400

    login y user admin

--- src/Utils/Context/AuthContext.tsx
+++ src/Utils/Context/AuthContext.tsx
@@ -1,11 +1,16 @@
 // context/AuthContext.tsx
-'use client'
+'use client';
 import React, { createContext, useContext, useEffect, useState, ReactNode } from 'react';
 import { onAuthStateChanged, User } from 'firebase/auth';
-import { auth } from '@/lib/firebase.js';
+import { auth, db } from '@/lib/firebase';
+import { collection, where, query, getDocs } from 'firebase/firestore';
 
 interface AuthContextType {
-  user: User | null;
+  user: IUserApp | null;
+}
+
+interface IUserApp extends User {
+  administrador: boolean;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -15,11 +20,30 @@ interface AuthProviderProps {
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<IUserApp | null>(null);
+
+  const checkIfAdmin = async (user: User): Promise<boolean> => {
+    try {
+      const administradoresRef = collection(db, "administradores");
+      const q = query(administradoresRef, where("administrador", "==", user.email));
+      const querySnapshot = await getDocs(q);
+      return !querySnapshot.empty;
+    } catch (error) {
+      console.error("Error getting documents: ", error);
+      return false;
+    }
+  };
+
+  
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      if (currentUser) {
+        const isAdmin = await checkIfAdmin(currentUser);
+        setUser({ ...currentUser, administrador: isAdmin });
+      } else {
+        setUser(null);
+      }
     });
 
     return () => unsubscribe();
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,52 @@
 'use client'
-import { signInWithGoogle } from '@/lib/firebase';
-import React from 'react';
+import { signInWithGoogle, auth } from '@/lib/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+
+
+interface IUsuario {
+  user: string,
+  pass: string
+}
 
 const Page = () => {
 
-  const handleGoogleSignIn = async() => {
+  const [usuario, setUsuario] = useState<IUsuario>({ user: '', pass: '' })
+  const router= useRouter()
+
+
+  const handleGoogleSignIn = async () => {
     try {
-        await signInWithGoogle()
+      await signInWithGoogle()
     } catch (error) {
-      console.log('error al inicial sesion en google ',error)
+      console.log('error al inicial sesion en google ', error)
     }
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target;
+    setUsuario((prevUsuario) => ({
+      ...prevUsuario,
+      [name]: value
+    }));
+  };
+
+  const handleSignIn = async (event: React.FormEvent<HTMLFormElement>) => {
+
+    event.preventDefault()
+    const { user, pass } = usuario
+    try {
+      await signInWithEmailAndPassword(auth, user, pass)
+      console.log('Inicio de sesión con correo y contraseña exitoso');
+      router.push("/")
+      
+    } catch (error) {
+      console.log(error)
+      alert('Error al iniciar sesión con correo y contraseña:');
+
+    }
+
   }
 
 
@@ -17,14 +54,16 @@ const Page = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Iniciar Sesión</h2>
-        <form>
+        <form onSubmit={handleSignIn}>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700 mb-2">Correo Electrónico</label>
             <input
               type="email"
               id="email"
-              name="email"
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              name="user"
+              value={usuario.user}
+              onChange={handleChange}
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none text-gray-800 focus:ring-2 focus:ring-blue-500"
               placeholder="Ingresa tu correo electrónico"
             />
           </div>
@@ -33,8 +72,10 @@ const Page = () => {
             <input
               type="password"
               id="password"
-              name="password"
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              name="pass"
+              value={usuario.pass}
+              onChange={handleChange}
+              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none  text-gray-800 focus:ring-2 focus:ring-blue-500"
               placeholder="Ingresa tu contraseña"
             />
           </div>
