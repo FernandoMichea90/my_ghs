059009c68681811935ca9ffbde273f203e7d5f18
Fernando Michea <fmicheam@gmail.com>
2024-09-02 21:29:13 -0400

    se agrego los regisros a la collection

--- src/app/documentos/page.tsx
+++ src/app/documentos/page.tsx
@@ -34,27 +34,26 @@ const page = () => {
   const searchParams = useSearchParams();
   const [loading, setLoading] = useState(true);
 
-
-
   useEffect(() => {
     const fetchData = async () => {
       console.log('Component mounted');
+      setLoading(true); // Comienza la carga
       try {
         let tag = searchParams.get('tag') || 'HDS';
-
+  
         const tagRef = doc(db, "Documentos", "InfoPrincipal");
         const tagDocSnap = await getDoc(tagRef);
-
+  
         if (tagDocSnap.exists()) {
           setHome(tagDocSnap.data() as IInfoPrincipal);
         } else {
           console.log("InfoPrincipal no encontrada");
         }
-
+  
         const documentCollectionRef = collection(db, "DocumentCollection");
         const q = query(documentCollectionRef, where("tag", "==", tag));
         const documentCollectionDocSnap = await getDocs(q);
-
+  
         if (!documentCollectionDocSnap.empty) {
           const documents = await Promise.all(
             documentCollectionDocSnap.docs.map(async (doc) => {
@@ -69,15 +68,19 @@ const page = () => {
         }
       } catch (error) {
         console.error("Error al obtener los datos: ", error);
+      } finally {
+        setLoading(false); // Termina la carga
       }
     };
-
+  
     fetchData();
-
+  
     return () => {
       console.log('Component unmounted');
     };
   }, [searchParams]);
+  
+
 
   const fetchDocuments = async (title: string) => {
     try {
@@ -93,7 +96,7 @@ const page = () => {
     } catch (error) {
       console.error("Error fetching documents: ", error);
       return []; // Return an empty array in case of error
-    } finally {
+    } finally {   
       console.log('Tarea finalizada');
     }
   };
@@ -102,50 +105,63 @@ const page = () => {
 
   return (
     <div className='flex-1 min-h-screen items-center justify-between text-gray-500 py-24 md:py-24 px-10 md:px-24'>
-
-      <div id='admin_documentos' className='flex items-center'>
-        <img src='icono_ghs/image.png' alt="" height={40} width={40}></img>
-        <h1 className='font-bold  text-2xl text-gray-900 mx-2'>
-          {home.titulo}
-        </h1>
-      </div>
-      <p className='mb-1 mt-3'>
-        {home.subtitulo}
-      </p>
-      <div className="flex flex-wrap gap-2 my-3">
-        {home.tags.map((tag, index) => (
-          <a href={'/documentos?tag=' + tag}>
-            <span key={index} className="bg-primary hover:bg-gray-900 text-white font-bold px-3 py-1 rounded-full text-sm">{tag}</span>
-          </a>
-        ))}
-      </div>
-
-      <div id='DocumentCollection' className='flex flex-wrap mt-20'>
-        <ul>
-          {DocumentCollection.map((doc: any, index) => doc.registros.length > 0 && (
-            <li className='list-none my-2'>
-              <span className='font-bold text-primary'>
-                {doc.titulo}
-              </span>
-
-              {doc.registros.length > 0 && doc.registros.map((e: any) => (
-                <ul>
-                  <li className='list-none my-2'>
-                    <a href={e.href} target='_blank'>
-                      <span className='text-primary font-semibold'>
-                        {'>> ' + e.titulo}
-                      </span>
-                    </a>
-                  </li>
-                </ul>
-              ))
-              }
-            </li>
-          ))}
-        </ul>
-      </div>
+      {loading ? (
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-primary font-semibold mb-4">Cargando datos...</p>
+          {/* Skeleton loading */}
+          <div className="animate-pulse space-y-4">
+            <div className="h-6 bg-gray-300 rounded w-3/4"></div>
+            <div className="h-6 bg-gray-300 rounded w-2/4"></div>
+            <div className="h-6 bg-gray-300 rounded w-1/4"></div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <div id='admin_documentos' className='flex items-center'>
+            <img src='icono_ghs/image.png' alt="" height={40} width={40}></img>
+            <h1 className='font-bold  text-2xl text-gray-900 mx-2'>
+              {home.titulo}
+            </h1>
+          </div>
+          <p className='mb-1 mt-3'>
+            {home.subtitulo}
+          </p>
+          <div className="flex flex-wrap gap-2 my-3">
+            {home.tags.map((tag, index) => (
+              <a href={'/documentos?tag=' + tag} key={index}>
+                <span className="bg-primary hover:bg-gray-900 text-white font-bold px-3 py-1 rounded-full text-sm">{tag}</span>
+              </a>
+            ))}
+          </div>
+  
+          <div id='DocumentCollection' className='flex flex-wrap mt-20'>
+            <ul>
+              {DocumentCollection.map((doc: any, index) => doc.registros.length > 0 && (
+                <li key={index} className='list-none my-2'>
+                  <span className='font-bold text-primary'>
+                    {doc.titulo}
+                  </span>
+  
+                  {doc.registros.length > 0 && doc.registros.map((e: any, eIndex: number) => (
+                    <ul key={eIndex}>
+                      <li className='list-none my-2'>
+                        <a href={e.href} target='_blank'>
+                          <span className='text-primary font-semibold'>
+                            {'>> ' + e.titulo}
+                          </span>
+                        </a>
+                      </li>
+                    </ul>
+                  ))}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   )
+  
 }
 
 export default page
