4ccb87ea538f1eac8625bb2da8741b1ef68591da
Fernando Michea <fmicheam@gmail.com>
2024-11-10 19:55:23 -0300

    [FEAT] component ButtonUrlFile

--- src/app/admin/edit_home/page.tsx
+++ src/app/admin/edit_home/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { db } from "@/lib/firebase";
 import { doc, getDoc, setDoc } from "firebase/firestore";
+import ButtonUrlFile from "@/components/ButtonUrlFile";
 
 export default function EditHome() {
   interface InfoHome {
@@ -121,20 +122,7 @@ export default function EditHome() {
             className="border p-2 w-full mb-4"
           />
         </div>
-        <div className="flex my-6 items-center">
-          <span className="text-red-500 px-1">
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M8 12h8M9 8H6a4 4 0 1 0 0 8h3m6-8h3a4 4 0 0 1 0 8h-3"/></svg>
-          </span>
-          <label className="block font-bold">Archivo Principal:</label>
-        </div>
-        <div>
-          <button className="bg-red-500  py-1 px-4 text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="m17.657 12l-1.414-1.414l2.121-2.122a2 2 0 1 0-2.828-2.828l-4.243 4.243a2 2 0 0 0 0 2.828l-1.414 1.414a4 4 0 0 1 0-5.657l4.242-4.242a4 4 0 0 1 5.657 5.657zM6.343 12l1.414 1.414l-2.121 2.122a2 2 0 1 0 2.828 2.828l4.243-4.243a2 2 0 0 0 0-2.828l1.414-1.414a4 4 0 0 1 0 5.657L9.88 19.778a4 4 0 1 1-5.657-5.657z"/></svg>
-          </button>
-          <button>
-          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024"><path fill="currentColor" d="M544 864V672h128L512 480L352 672h128v192H320v-1.6c-5.376.32-10.496 1.6-16 1.6A240 240 0 0 1 64 624c0-123.136 93.12-223.488 212.608-237.248A239.81 239.81 0 0 1 512 192a239.87 239.87 0 0 1 235.456 194.752c119.488 13.76 212.48 114.112 212.48 237.248a240 240 0 0 1-240 240c-5.376 0-10.56-1.28-16-1.6v1.6z"/></svg>
-          </button>
-        </div>
+        <ButtonUrlFile></ButtonUrlFile>
         <input
           name="src"
           value={infoHome.src}
